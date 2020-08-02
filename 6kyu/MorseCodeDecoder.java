6389297297511f79b52c977235d9bfc3901329ed
辻真緒 <>
2020-08-02 21:29:24 +0900

    add another answers

--- 6kyu/MorseCodeDecoder.java
+++ 6kyu/MorseCodeDecoder.java
@@ -9,4 +9,23 @@ public class MorseCodeDecoder {
         .map(e -> Stream.of(e.split(" ")).map(MorseCode::get).collect(Collectors.joining()))
         .collect(Collectors.joining(" "));
   }
+
+  public static String decode1(String morseCode) {
+    return Stream.of(morseCode.trim().split("   ")).map(MorseCodeDecoder::decodeWords).collect(Collectors.joining(" "));
+  }
+
+  public static String decodeWords(String word) {
+    return Stream.of(e.split(" ")).map(MorseCode::get).collect(Collectors.joining());
+  }
+
+  public static String decode2(String morseCode) {
+    String result = "";
+    for (String word : morseCode.trim().split("   ")) {
+      for (String letter : word.split("\\s+")) {
+        result += MorseCode.get(letter);
+      }
+      result += ' ';
+    }
+    return result.trim();
+  }
 }
